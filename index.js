36549ea158c75316476719f0c29cf53793ff1dd3
Miguel Velásquez <vmiguel.xy@gmail.com>
2022-12-01 16:34:12 -0400

    Variable de Entorno para los Puertos de Deploy mediante Railway

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routerApi = require("./routes");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.listen(port, () => { console.log(`El servidor está corriendo en el puerto ${port}`); });
 routerApi(app);
